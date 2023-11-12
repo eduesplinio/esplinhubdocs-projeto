1f711a4565cd980a29b8436241f99cbd1189162c
Eduardo Esplinio <esplinioo@gmail.com>
2023-11-12 09:20:17 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import {
+    sync
+} from 'glob';
+import {
+    defineConfig
+} from 'vite';
+
+export default defineConfig({
+    base: '',
+    root: path.resolve(__dirname, 'src'),
+    build: {
+        rollupOptions: {
+            input: sync(path.resolve(__dirname, 'src/**/*.{html,css,js}')),
+            preserveEntrySignatures: true
+        },
+        outDir: '../dist',
+        emptyOutDir: true
+    }
+});
